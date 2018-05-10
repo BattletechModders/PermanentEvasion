614ff7f664bd49c2631af49b9dee24e9cd7e37fd
Morphyum <zion@c-schwabe.de>
2018-05-11 00:47:49 +0200

    *first version

---
+++ PermanentEvasion/PermanentEvasion/Patch.cs
@@ -0,0 +1,45 @@
+﻿using BattleTech;
+using BattleTech.UI;
+using Harmony;
+using System;
+using System.Collections.Generic;
+
+namespace PermanentEvasion {
+
+    [HarmonyPatch(typeof(AbstractActor), "ResolveAttackSequence")]
+    public static class AbstractActor_ResolveAttackSequence {
+
+        static bool Prefix(AbstractActor __instance) {
+            return false;
+        }
+
+        static void Postfix(AbstractActor __instance, string sourceID, int sequenceID, int stackItemID, AttackDirection attackDirection) {
+            try {
+
+                AttackDirector.AttackSequence attackSequence = __instance.Combat.AttackDirector.GetAttackSequence(sequenceID);
+                if (attackSequence != null && attackSequence.attackDidDamage) {
+                    List<Effect> list = __instance.Combat.EffectManager.GetAllEffectsTargeting(__instance).FindAll((Effect x) => x.EffectData.targetingData.effectTriggerType == EffectTriggerType.OnDamaged);
+                    for (int i = 0; i < list.Count; i++) {
+                        list[i].OnEffectTakeDamage(attackSequence.attacker, __instance);
+                    }
+                    if (attackSequence.isMelee) {
+                        int value = attackSequence.attacker.StatCollection.GetValue<int>("MeleeHitPushBackPhases");
+                        if (value > 0) {
+                            for (int j = 0; j < value; j++) {
+                                __instance.ForceUnitOnePhaseDown(sourceID, stackItemID, false);
+                            }
+                        }
+                    }
+                }
+                int evasivePipsCurrent = __instance.EvasivePipsCurrent;
+                int evasivePipsCurrent2 = __instance.EvasivePipsCurrent;
+                if (evasivePipsCurrent2 < evasivePipsCurrent && !__instance.IsDead && !__instance.IsFlaggedForDeath) {
+                    __instance.Combat.MessageCenter.PublishMessage(new FloatieMessage(__instance.GUID, __instance.GUID, "-1 EVASION", FloatieMessage.MessageNature.Debuff));
+                }
+            }
+            catch (Exception e) {
+                Logger.LogError(e);
+            }
+        }
+    }
+}
---
+++ PermanentEvasion/PermanentEvasion/PermanentEvasion.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace PermanentEvasion
+{
+    public class PermanentEvasion
+    {
+        public static void Init() {
+            var harmony = HarmonyInstance.Create("de.morphyum.BrokenSalvagedMechs");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
